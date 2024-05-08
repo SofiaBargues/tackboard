8359860b95e1fa412b42f5928daa7956e2a3d280
Sofia Bargues <barguessofia@gmail.com>
2024-05-08 01:05:35 +0100

    FLEXBOX HEELL

--- components/shared/Todo.tsx
+++ components/shared/Todo.tsx
@@ -4,21 +4,23 @@ import EditTodo from "./EditTodo";
 import { useState } from "react";
 const Todo = ({ todo }: { todo: TodoType }) => {
   return (
-    <div className="flex flex-col items-center">
-      <div className="p-6 flex flex-col justify-around shadow-2xl shadow-amber-950 min-w-80 w-80 h-80 bg-background ">
+    <div className="p-6 flex flex-col justify-between shadow-2xl shadow-amber-950 min-w-80 w-80 h-80 bg-background ">
+      <div className=" w-full h-16 flex justify-center items-start ">
         <div
-          className="m-auto h-8 w-8 rounded-full shadow-2xl shadow-amber-950 inline-block"
+          className="h-8 w-8 rounded-full"
           style={{
             backgroundColor: todo.backgroundColor || "red",
           }}
         ></div>
+      </div>
+      <div className="flex flex-col justify-evenly items-center">
         <span className="text-center">
           <h1 className="font-bold uppercase text-2xl">{todo.title}</h1>
           <p>{todo.mensaje}</p>
         </span>
-        <div className="flex justify-end gap-5 text-xl mt-10">
-          <EditTodo todo={todo} />
-        </div>
+      </div>
+      <div className="flex justify-end gap-5 text-xl mt-10">
+        <EditTodo todo={todo} />
       </div>
     </div>
   );
